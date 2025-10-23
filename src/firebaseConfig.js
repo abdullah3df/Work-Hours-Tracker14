386fcbffed8be43751068435430dbeaf5fffb621
abdullah3df <57577059+abdullah3df@users.noreply.github.com>
2025-10-23 19:15:15 +0200

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+// src/firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "ضع هنا المفتاح الجديد من Google Cloud Console",
+  authDomain: "saaati-login.firebaseapp.com",
+  projectId: "saaati-login",
+  storageBucket: "saaati-login.appspot.com",
+  messagingSenderId: "711755388111",
+  appId: "1:711755388111:web:f130d27854bc9f32e253ba"
+};
